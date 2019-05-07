d2fe62e2cf3d5384ccb73dc7732eb4098a90d83a
tywi6665 <tyletcole@gmail.com>
2019-05-07 15:09:33 -0600

    refining linear gradients

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -3,10 +3,12 @@ import "./Tile.scss";
 
 const Tile = (props) => (
     <div className={`tile tile${props.className}`}>
+    <div className={`innerTile innerTile${props.className}`}>
         <h2 className={`tileTitle tileTitle${props.className}`}>{props.title}</h2>
         <h3>{props.something}</h3>
         <h3>{props.something}</h3>
         <h3>{props.something}</h3>
+     </div>   
     </div>
 );
 
